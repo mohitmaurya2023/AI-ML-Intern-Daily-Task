d8d5c63e65580a8622d6c773f3ecdd04220f70bf
Mohit Maurya <74113320+mohitmaurya2023@users.noreply.github.com>
2024-08-20 16:52:38 +0530

    Delete Week-14/16-Aug-2024 directory

--- Week-14/16-Aug-2024/Aboutpage.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.write("Welcome to About Page.")
-st.write_stream(callable)
-st.bokeh_chart()
-st.balloons()
--- Week-14/16-Aug-2024/dashboard.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.write("Welcome to Dashborad.")
-st.write_stream(callable)
-st.bokeh_chart()
-st.balloons()
